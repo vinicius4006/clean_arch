e2880f64d824eba92b28e9f4aecdc2fd8672450b
vinicius4006 <vfranca477@gmail.com>
2022-11-03 15:48:53 -0300

    refactoring

---
+++ lib/layers/core/inject/inject.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+
+class Inject {
+  static void init() {
+    GetIt getIt = GetIt.instance;
+
+    // datasources
+    // repositories
+    // usecases
+    // controllers
+  }
+}
